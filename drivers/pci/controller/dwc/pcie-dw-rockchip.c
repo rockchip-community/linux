6bcec32669a03afc9834e2f93fb1d51eebb6a478
Sebastian Reichel <sebastian.reichel@collabora.com>
2025-11-24 20:06:39 +0100

    [WIP] PCI: dw-rockchip: port some suspend code from vendor kernel
    The vendor kernel does these calls before starting the actual process of
    going into L2 state. I have not yet looked into the details to come up
    with a proper patch description, but this seems to fix up the L2 timeout
    visible with the network card on the RK3588 ROCK 5B(+).
    
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/pci/controller/dwc/pcie-dw-rockchip.c
+++ drivers/pci/controller/dwc/pcie-dw-rockchip.c
@@ -74,6 +74,9 @@
 #define  PCIE_CLKREQ_NOT_READY		FIELD_PREP_WM16(BIT(0), 0)
 #define  PCIE_CLKREQ_PULL_DOWN		FIELD_PREP_WM16(GENMASK(13, 12), 1)
 
+/* General Debug Register */
+#define PCIE_CLIENT_GENERAL_DEBUG	0x104
+
 /* Hot Reset Control Register */
 #define PCIE_CLIENT_HOT_RESET_CTRL	0x180
 #define  PCIE_LTSSM_APP_DLY2_EN		BIT(1)
@@ -810,6 +813,12 @@ disable_regulator:
 	return ret;
 }
 
+
+static inline void rockchip_pcie_link_status_clear(struct rockchip_pcie *rockchip)
+{
+	rockchip_pcie_writel_apb(rockchip, PCIE_CLIENT_GENERAL_DEBUG, 0x0);
+}
+
 static int rockchip_pcie_suspend(struct device *dev)
 {
 	struct rockchip_pcie *rockchip = dev_get_drvdata(dev);
@@ -822,6 +831,11 @@ static int rockchip_pcie_suspend(struct device *dev)
 	}
 
 	rockchip->intx = rockchip_pcie_readl_apb(rockchip, PCIE_CLIENT_INTR_MASK_LEGACY);
+
+	/* All sub-devices are in D3hot by PCIe stack */
+	dw_pcie_dbi_ro_wr_dis(pci);
+
+	rockchip_pcie_link_status_clear(rockchip);
 
 	ret = dw_pcie_suspend_noirq(pci);
 	if (ret)
